58f17b94a250793a90f4eb0008cbaa53cf5e85b9
LordChronic <kainalu210@gmail.com>
2024-04-30 21:27:58 -0700

    bruh

--- nuka-zukes/src/App.js
+++ nuka-zukes/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please don't fire me, Ryan :(
         </p>
         <a
           className="App-link"
